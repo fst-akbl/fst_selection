845a4c97d71e80c02c792c98b4c10b8b7a5ad7ce
LHG0014 <1102526044@qq.com>
2020-01-14 13:37:15 +0800

    init

---
+++ src/test/java/com/course_selection/CourseSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course_selection;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSelectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
